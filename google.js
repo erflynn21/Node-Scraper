08159d0fa07eb8da52ce6ffcdc037e76d2216995
Evan Flynn <erflynn21@gmail.com>
2022-05-04 12:06:00 +0800

    adds parsing of Google search results

--- google.js
+++ google.js
@@ -17,7 +17,7 @@ const googleRequest = async (request) => {
 
         let username = process.env.USERNAME;
         if (request.geo !== 'none') {
-            username = `${process.env.USERNAME}-country-${request.geo}`;
+            username = `${process.env.USERNAME}-country-${request.geo}-timeoutSeconds-60`;
         }
         await page.authenticate({
             username,
@@ -36,8 +36,9 @@ const googleRequest = async (request) => {
         let paidTitles = [];
         let paidDescriptions = [];
         let paidShownURLs = [];
+        let allTitles = [];
 
-        $('.v5yQqb > a').each((i, el) => {
+        $('.sVXRqc').each((i, el) => {
             paidURLs[i] = $(el).attr('href');
         })
 
@@ -45,13 +46,17 @@ const googleRequest = async (request) => {
             paidTitles[i] = $(el).text();
         });
 
-        $('.MUxGbd div span').each((i, el) => {
+        $('.MUxGbd div').each((i, el) => {
             paidDescriptions[i] = $(el).text();
         });
 
         $('.v5yQqb a .x2VHCd').each((i, el) => {
             paidShownURLs[i] = $(el).text();
         });
+
+        $('.CCgQ5 span, .NJo7tc > .yuRUbf > a > h3').each((i, el) => {
+            allTitles[i] = $(el).text();
+        })
 
         const paidResults = [];
         for (let i = 0; i < paidURLs.length; i++) {
@@ -60,10 +65,50 @@ const googleRequest = async (request) => {
                 description: paidDescriptions[i],
                 url: paidURLs[i],
                 shownURL: paidShownURLs[i],
+                position: i + 1,
+                overllPosition: allTitles.indexOf(paidTitles[i]) + 1,
             }
         }
 
         console.log(paidResults);
+
+        let organicURLs = [];
+        let organicTitles = [];
+        let organicDescriptions = [];
+        let organicShownURLs = [];
+
+        $('.NJo7tc > .yuRUbf > a > h3').each((i, el) => {
+            organicTitles[i] = $(el).text();
+        });
+
+        // console.log(organicTitles);
+
+        $('.VwiC3b span').each((i, el) => {
+            organicDescriptions[i] = $(el).text();
+        });
+
+        $('.NJo7tc > .yuRUbf > a').each((i, el) => {
+            organicURLs[i] = $(el).attr('href');
+        })
+
+        $('.NJo7tc > .yuRUbf > a > .TbwUpd > .iUh30').each((i, el) => {
+            organicShownURLs[i] = $(el).text();
+        });
+
+        const organicResults = [];
+        for (let i = 0; i < organicURLs.length; i++) {
+            organicResults[i] = {
+                title: organicTitles[i],
+                description: organicDescriptions[i],
+                url: organicURLs[i],
+                shownURL: organicShownURLs[i],
+                position: i + 1,
+                overallPosition: allTitles.indexOf(organicTitles[i]) + 1,
+            }
+        }
+
+        // console.log('organic');
+        console.log(organicResults);
 
         // await browser.close()
         resolve(content);
